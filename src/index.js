0ea2f931f8a72dadb3a8066794540a9ab0c6bc52
Gachuz <sergioegch@hotmail.com>
2022-09-15 02:57:15 -0500

    Diseño de vista SignIn y SignUp para desktop

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ app.use(passport.session());
 //* Variables Globales
 app.use((req, res, next) =>{
     app.locals.success = req.flash('success');
-    app.locals.success = req.flash('message');
+    app.locals.message = req.flash('message');
     app.locals.user = req.user;
     next();
 })

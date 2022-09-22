6e807e821ea6e9332c4e65801950e2e4f1aaf57f
Gachuz <sergioegch@hotmail.com>
2022-09-21 22:47:26 -0500

    Fix de usuario existente, optimizacion de menu y se añadio titulo en login

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -6,6 +6,12 @@ var side__menu = document.getElementById('menu__side');
 var btn__open = document.getElementById('btn__open');
 var body = document.getElementById('body');
 
+var miperfil = document.getElementById('miperfil');
+var partida = document.getElementById('partida');
+var proveedor = document.getElementById('proveedor');
+var contrato = document.getElementById('contrato');
+var solicitud = document.getElementById('solicitud');
+
 //Ocultar y mostrar menu
 function open_close_menu(){
     body.classList.toggle("body__move");
@@ -27,4 +33,42 @@ window.addEventListener("resize", function() {
         body.classList.add("body__move");
         side__menu.classList.add("menu__side__move");
     }
+});
+
+$(document).ready(function () {
+    if(window.location.href.indexOf("miperfil") > -1) {
+        miperfil.classList.add('selected');
+        partida.classList.remove('selected');
+        proveedor.classList.remove('selected');
+        contrato.classList.remove('selected');
+        solicitud.classList.remove('selected');
+    }
+    if(window.location.href.indexOf("partida") > -1) {
+       miperfil.classList.remove('selected');
+       partida.classList.add('selected');
+       proveedor.classList.remove('selected');
+       contrato.classList.remove('selected');
+       solicitud.classList.remove('selected');
+    }
+    if(window.location.href.indexOf("proveedor") > -1) {
+        miperfil.classList.remove('selected');
+        partida.classList.remove('selected');
+        proveedor.classList.add('selected');
+        contrato.classList.remove('selected');
+        solicitud.classList.remove('selected');
+     }
+     if(window.location.href.indexOf("contrato") > -1) {
+        miperfil.classList.remove('selected');
+        partida.classList.remove('selected');
+        proveedor.classList.remove('selected');
+        contrato.classList.add('selected');
+        solicitud.classList.remove('selected');
+     }
+     if(window.location.href.indexOf("solicitud") > -1) {
+        miperfil.classList.remove('selected');
+        partida.classList.remove('selected');
+        proveedor.classList.remove('selected');
+        contrato.classList.remove('selected');
+        solicitud.classList.add('selected');
+     }
 });

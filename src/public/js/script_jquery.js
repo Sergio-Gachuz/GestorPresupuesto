61bc1744c29afd109debb2656dbc20f95af8db07
Gachuz <sergioegch@hotmail.com>
2022-10-02 00:28:57 -0500

    Logica para solicitudes de pagos, refactor de vistas de solicitud de pago, adicion de sweetalert2

--- src/public/js/script_jquery.js
+++ src/public/js/script_jquery.js
@@ -1,4 +1,5 @@
 $(document).ready( function () {
+    // Configuración de DataTables
     $('#table').DataTable({
         dom: 'Bfrtip',
         ordering: true,
@@ -20,6 +21,7 @@ $(document).ready( function () {
     });    
 });
 
+// Limitar caracteres
 $('input[name="rfc"]').keypress(function() { 
     if (this.value.length >= 13 ) { 
         return false; 
@@ -31,3 +33,59 @@ $('input[name="codigo_postal"]').keypress(function() {
         return false; 
     }
 });
+
+// Funciones para SweetAlert2
+function eliminarRegistro(route, id) {
+    Swal.fire({
+        title: '¿Está seguro de eliminar este registro?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Confirmar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            url = route + id;
+            window.location = url;
+        }
+    })
+}
+async function guardarRegistro(event, form, titulo, texto = null) {
+    event.preventDefault();
+    Swal.fire({
+        title: titulo,
+        text: texto,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Confirmar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            form.submit();
+        }
+    })
+}
+
+function generarPDF(){
+    let timerInterval
+    Swal.fire({
+        title: 'Generando PDF',
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading()
+            const b = Swal.getHtmlContainer().querySelector('b')
+            timerInterval = setInterval(() => {
+                b.textContent = Swal.getTimerLeft()
+            }, 100)
+        },
+        willClose: () => {
+            clearInterval(timerInterval)
+        }
+    }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+            //Aqui va el codigo para generar el PDF
+        }
+    })
+}
--- src/routes/solicitud_pago.js
+++ src/routes/solicitud_pago.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { isLoggedIn } = require('../lib/auth');
-const { obtenerVistaNuevoSolicitud_pago, añadirSolicitud_pago, obtenerSolicitud_pagos, eliminarSolicitud_pago, obtenerSolicitud_pago, editarSolicitud_pago } = require('../controller/solicitud_pago');
+const { obtenerVistaNuevoSolicitud_pago, obtenerPartidaProveedor, añadirSolicitud_pago, obtenerSolicitud_pagos, eliminarSolicitud_pago, obtenerSolicitud_pago, editarSolicitud_pago } = require('../controller/solicitud_pago');
 
 router.get('/nuevo', isLoggedIn, obtenerVistaNuevoSolicitud_pago );
+
+router.get('/partida_proveedor/:proveedor_id', obtenerPartidaProveedor)
 
 router.post('/nuevo', isLoggedIn, añadirSolicitud_pago);
 
 router.get('/', isLoggedIn, obtenerSolicitud_pagos)
 
-router.get('/borrar/:no_folio', isLoggedIn, eliminarSolicitud_pago)
+router.get('/borrar/:solicitud_id', isLoggedIn, eliminarSolicitud_pago)
 
-router.get('/editar/:no_folio', isLoggedIn, obtenerSolicitud_pago)
+router.get('/editar/:solicitud_id', isLoggedIn, obtenerSolicitud_pago)
 
-router.post('/editar/:no_folio', isLoggedIn, editarSolicitud_pago)
+router.post('/editar/:solicitud_id', isLoggedIn, editarSolicitud_pago)
 
 module.exports = router;
